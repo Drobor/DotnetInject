1f0b2e7532dea91b79441ca802bba7286dc048f0
Drobor <m295-6d@ya.ru>
2022-12-16 18:28:35 +0300

    fixed initialization issues caused by it being launched from DllMain

--- DotnetInject.Injector/ClrInjector.cs
+++ DotnetInject.Injector/ClrInjector.cs
@@ -3,6 +3,7 @@ using System.Diagnostics;
 using System.IO;
 using System.IO.MemoryMappedFiles;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Text.Json;
 using System.Threading;
@@ -11,6 +12,34 @@ namespace DotnetInject.Injector
 {
     public class ClrInjector : IClrInjector
     {
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern IntPtr CreateRemoteThread(
+            IntPtr hProcess,
+            IntPtr lpThreadAttributes,
+            UIntPtr dwStackSize,
+            IntPtr lpStartAddress,
+            IntPtr lpParameter,
+            CREATE_THREAD_FLAGS dwCreationFlags,
+            out uint lpThreadId);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern uint WaitForSingleObject(IntPtr hHandle, uint dwMilliseconds);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool GetExitCodeThread(IntPtr hThread, out uint lpExitCode);
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        public static extern uint GetSystemWow64Directory(StringBuilder lpBuffer, uint uSize);
+
+        [Flags]
+        public enum CREATE_THREAD_FLAGS
+        {
+            RUN_IMMEDIATELY = 0,
+            CREATE_SUSPENDED = 4,
+            STACK_SIZE_PARAM_IS_A_RESERVATION = 65536, // 0x00010000
+        }
+
         public void Inject(Process process, string pathToInjectingAssembly, string entryPointAssemblyQualifiedName, string? pathToHostFxr = null, string? runtimeConfigPath = null)
             => InjectInternal(process, pathToInjectingAssembly, entryPointAssemblyQualifiedName, "", pathToHostFxr, runtimeConfigPath);
 
@@ -42,7 +71,7 @@ namespace DotnetInject.Injector
             var bw = new BinaryWriter(stream, Encoding.Unicode); //(wchar_t* hostFxrPath, wchar_t* runtime_config_path, wchar_t* assemblyDllPath, wchar_t* initArgs)
             bw.WriteCString(pathToHostFxr);
             bw.WriteCString(runtimeConfigPath);
-            bw.WriteCString(pathToInjectingAssembly);
+            bw.WriteCString(Path.GetFullPath(pathToInjectingAssembly));
             bw.WriteCString(entryPointAssemblyQualifiedName);
             bw.WriteCString(entryPointArgs);
 
@@ -52,6 +81,21 @@ namespace DotnetInject.Injector
             Console.ReadLine();
 
             var injectresult = nativeInjector.Inject(Path.GetFullPath("DotnetInject.Native.dll"));
+
+            stream.Position = 10235;
+            var br = new BinaryReader(stream);
+
+            var address = br.ReadInt32();
+            var signalByte = br.ReadByte();
+
+            if (signalByte != 255)
+                throw new Exception("Signal byte that was supposed to be set by native payload wasn't set");
+            
+            var newThread = CreateRemoteThread(process.Handle, IntPtr.Zero, UIntPtr.Zero, (nint)address, IntPtr.Zero, CREATE_THREAD_FLAGS.RUN_IMMEDIATELY, out uint _);
+            //Console.WriteLine("WAiting for input to proceed with injection");
+            //Console.ReadLine();
+            WaitForSingleObject(newThread, uint.MaxValue);
+
 
             for (int i = 0; i < 1000; i++)
             {
--- DotnetInject.Native/dllmain.c
+++ DotnetInject.Native/dllmain.c
@@ -2,7 +2,20 @@
 
 #include "ClrStarter.h"
 
-void LoadFromMemoryMappedFile()
+void* _pBuf;
+HANDLE _hMapFile;
+
+__declspec(dllexport) int __stdcall LoadClrFromMemoryMappedFile(LPVOID lpParam)
+{
+    LoadRuntimePacked((wchar_t*)_pBuf);
+
+    ((char*)_pBuf)[0] = 0;
+
+    UnmapViewOfFile(_pBuf);
+    CloseHandle(_hMapFile);
+}
+
+void LoadMemoryMappedFile()
 {
     wchar_t processId[32] = L"";
 
@@ -11,32 +24,30 @@ void LoadFromMemoryMappedFile()
     wchar_t memoryMappedFileName[64] = L"DotnetInjectBootstrapperParameters-";
     wcscat_s(memoryMappedFileName, 64, processId);
 
-    HANDLE hMapFile = OpenFileMappingW(
+    _hMapFile = OpenFileMappingW(
         FILE_MAP_ALL_ACCESS, // read/write access
         FALSE, // do not inherit the name
         memoryMappedFileName); // name of mapping object
 
-    if (hMapFile == NULL)
+    if (_hMapFile == NULL)
         return;
 
-    void* pBuf = MapViewOfFile(hMapFile, // handle to map object
-                               FILE_MAP_ALL_ACCESS, // read/write permission
-                               0,
-                               0,
-                               10240);
+    _pBuf = MapViewOfFile(_hMapFile, // handle to map object
+                          FILE_MAP_ALL_ACCESS, // read/write permission
+                          0,
+                          0,
+                          10240);
 
-    if (pBuf == NULL)
+    if (_pBuf == NULL)
     {
-        CloseHandle(hMapFile);
+        CloseHandle(_hMapFile);
         return;
     }
 
-    LoadRuntimePacked((wchar_t*)pBuf);
+    *(((unsigned char*)_pBuf) + 10239) = 255;
 
-    *(char*)pBuf = 0;
-
-    UnmapViewOfFile(pBuf);
-    CloseHandle(hMapFile);
+    int* functionAddress = (int*)(((char*)_pBuf) + 10235);
+    *functionAddress = (int)&LoadClrFromMemoryMappedFile;
 }
 
 int APIENTRY DllMain(HMODULE hModule,
@@ -47,7 +58,7 @@ int APIENTRY DllMain(HMODULE hModule,
     switch (ul_reason_for_call)
     {
     case DLL_PROCESS_ATTACH:
-        LoadFromMemoryMappedFile();
+        LoadMemoryMappedFile();
     case DLL_THREAD_ATTACH:
     case DLL_THREAD_DETACH:
     case DLL_PROCESS_DETACH:
